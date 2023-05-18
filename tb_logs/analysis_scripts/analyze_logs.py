984bf951032ce2fb784a0c8eb217bf060c4fc2ec
Adam Lagoda <adam.lagoda.w@gmail.com>
2023-05-18 17:30:47 +0200

    reformat

--- tb_logs/analysis_scripts/analyze_logs.py
+++ tb_logs/analysis_scripts/analyze_logs.py
@@ -1,18 +1,17 @@
-import os
-import csv
-from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 from pathlib import Path
+
 from lib.extract_csv_tensor import extract_data_from_tensorboard
 from lib.plot_csv import plot_csv_logs
+
 
 log_folder = "Torque_Reward_and_with_lost_detection_test_1_1683889158.8175397_1"
 
 # Usage example
 rollout = [
-    'rollout/ep_len_mean',
-    'rollout/ep_rew_mean',
-    'rollout/exploration_rate',
-    'train/loss'
+    "rollout/ep_len_mean",
+    "rollout/ep_rew_mean",
+    "rollout/exploration_rate",
+    "train/loss",
 ]
 
 log_dir = Path(__file__).parent.parent / log_folder
